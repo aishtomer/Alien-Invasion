5c761604cdd34d947aa54503ab494677743a9c90
aishtomer <91372700+aishtomer@users.noreply.github.com>
2022-12-19 18:23:07 +0530

    commit

--- button.py
+++ button.py
@@ -2,3 +2,14 @@ class Button:
     
     def __init__(self, ai_game) -> None:
         """Initialise button attributes"""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        # Set the dimensions and properties of the button
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Build the button's rect object and center it.
+        
